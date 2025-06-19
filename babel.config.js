71379733014f7cdcf20ed470d59c385fecd3e362
drilonademaj-ci <d.ademaj@code-invention.com>
2025-06-19 10:15:06 +0200

    deployment fix

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,5 @@ module.exports = {
     "@babel/preset-env",
     ["@babel/preset-react", { runtime: "automatic" }], // automatic JSX transform for React 19
   ],
+  plugins: ["@babel/plugin-syntax-import-attributes"],
 };
